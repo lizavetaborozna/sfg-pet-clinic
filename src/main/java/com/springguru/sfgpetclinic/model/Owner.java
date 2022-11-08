899d5646cb4da32a2ceb1d72d30444ef52c60df2
PK <pavel.khmialinku@standardbank.co.za>
2022-11-08 17:50:38 +0300

    Add POJOs

---
+++ src/main/java/com/springguru/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.springguru.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/springguru/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.springguru.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
