aa245fddd658ea0db637a8f3a97d82899787899e
Lizaveta Barazna <lizavetaborozna@gmail.com>
2022-11-09 17:58:25 +0300

    Refactor service interfaces

--- pet-clinic-data/src/main/java/com/springguru/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/springguru/sfgpetclinic/services/PetService.java
@@ -3,12 +3,6 @@ package com.springguru.sfgpetclinic.services;
 
 import com.springguru.sfgpetclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
--- pet-clinic-data/src/main/java/com/springguru/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/springguru/sfgpetclinic/services/VetService.java
@@ -3,12 +3,6 @@ package com.springguru.sfgpetclinic.services;
 
 import com.springguru.sfgpetclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
