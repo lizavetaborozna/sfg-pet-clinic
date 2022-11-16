5143b4476bc3e8e010942183a96cf35fdb37ba16
Lizaveta Barazna <lizavetaborozna@gmail.com>
2022-11-16 23:22:08 +0300

    Add pet type map service

---
+++ pet-clinic-data/src/main/java/com/springguru/sfgpetclinic/services/map/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.springguru.sfgpetclinic.services.map;
+
+import com.springguru.sfgpetclinic.model.PetType;
+import com.springguru.sfgpetclinic.services.CrudService;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
