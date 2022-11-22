5596ca7df9077e793105f34158b60a312b7518ef
Lizaveta Barazna <lizavetaborozna@gmail.com>
2022-11-22 23:12:48 +0300

    Update visit with JPA entity mappings

--- pet-clinic-data/src/main/java/com/springguru/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/springguru/sfgpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package com.springguru.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -19,6 +21,17 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
+    }
 
     public String getName() {
         return name;
